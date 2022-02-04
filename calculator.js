b7e5cb2459c0a2f6d13b744b4b0b3698270bc05f
yavorant <yavorant@gmail.com>
2022-02-05 00:33:23 +0200

    adding functionality works

--- calculator.js
+++ calculator.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', function (req, res) {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+
+  let result = num1 + num2;
+
+  res.send(`The result of the calculation is ${result}`);
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
